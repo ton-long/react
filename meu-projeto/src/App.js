a99075ef4ec7ab0f81d32234d5b9ed0595a4dce7
ton-long <114124041+ton-long@users.noreply.github.com>
2023-06-07 01:09:45 -0300

    Entendendo o JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+  const name = 'Luã'
+  const newName = name.toUpperCase()
+
+  function som(a, b){
+    return a+b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
-      <p>Meu primeiro app.</p>
+      <h1>Olá Mundo! </h1>
+      <p>Meu primeiro app. </p>
+      <h2>Alterando o JSX </h2>
+      <p>Olá, {name}! </p>
+      <p>Seu nome em letras maiúsculas: {newName}. </p>
+      <p>Soma: {som(1, 3)} </p>
+      <img src={url} alt='minha imagem' />
     </div>
   );
 }
