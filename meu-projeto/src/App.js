1c5799f9e3ac6ab0d1d8e2cbcc427c41d0ff8374
ton-long <114124041+ton-long@users.noreply.github.com>
2023-08-23 15:20:47 -0300

    React Router

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -10,7 +10,13 @@ import Condicional from './components/Condicional';
 import OutraLista from './components/OutraLista';
 import SeuNome from './components/SeuNome';
 import Saudacao from './components/Saudacao';
-
+//React Router
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Empresa from './Pages/Empresa';
+import Contato from './Pages/Contato';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
@@ -78,6 +84,17 @@ function App() {
       <h1>State Lift</h1>
       <SeuNome setNome={setNome} />
       <Saudacao nome={nome}/>
+      
+      <h1>React Router exemplo:</h1>
+      <Router> 
+        <NavBar/>           
+        <Routes>
+            <Route exact path="/" element={ <Home/> } />
+            <Route path="/empresa" element={ <Empresa/> } />
+            <Route path="/contato" element={ <Contato/>} />            
+        </Routes> 
+        <Footer/>       
+      </Router>
     </div> 
   );  
 }
